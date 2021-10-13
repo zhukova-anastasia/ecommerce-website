3db1604decb19430b3c5d8ba99f5bdd74c410a4a
Anastasia Zhukova <88766176+zhukova-anastasia@users.noreply.github.com>
2021-10-13 15:20:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//MENU SHOW
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+//REMOVE MOBILE MENU
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
